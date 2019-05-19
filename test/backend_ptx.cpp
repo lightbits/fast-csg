3adeb791ac0467471101e984ae0226c585e796a7
lightbits <simenh.jobb@gmail.com>
2019-05-19 18:44:08 +0200

    simplify code

--- test/backend_ptx.cpp
+++ test/backend_ptx.cpp
@@ -9,20 +9,20 @@
 #include "../src/frep_eval.h"
 #include "../src/frep_builder.h"
 #include "../src/backend_ptx.h"
-#include "util/ptxjit.h"
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include "util/cuda_error.h"
 
-/*
-This generates a PTX program equivalent to
-
-  float f(float x, float y, float z);
-  void main(vec4 *input, float *output) {
-      int tid = threadIdx.x + blockDim.x*blockIdx.x;
-      vec4 p = input[tid];
-      output[tid] = f(p.x, p.y, p.z);
-  }
-
-Note: out_length _DOES NOT_ include the null-terminator.
-*/
+// This generates a PTX program equivalent to:
+//   float tree(float x, float y, float z) {
+//       // generated PTX instructions
+//   }
+//   void main(vec4 *input, float *output) {
+//       int tid = threadIdx.x + blockDim.x*blockIdx.x;
+//       vec4 p = input[tid];
+//       output[tid] = tree(p.x, p.y, p.z);
+//   }
+// Note: out_length _DOES NOT_ include the null-terminator.
 char *generate_ptx_program(frep_t *f, size_t *out_length)
 {
     const char *ptx_template = R"str(
@@ -46,9 +46,9 @@ char *generate_ptx_program(frep_t *f, size_t *out_length)
         ld.param.u64 rd2, [param1];
         cvta.to.global.u64 rd3, rd2;
         cvta.to.global.u64 rd4, rd1;
-        mov.u32 r1, %%tid.x;         // threadIdx.x
-        mov.u32 r2, %%ctaid.x;       // blockIdx.x
-        mov.u32 r3, %%ntid.x;        // blockDim.x
+        mov.u32 r1, %%tid.x;       // threadIdx.x
+        mov.u32 r2, %%ctaid.x;     // blockIdx.x
+        mov.u32 r3, %%ntid.x;      // blockDim.x
         mad.lo.s32 r4, r3, r2, r1; // blockDim.x*blockIdx.x + threadIdx.x
         mul.wide.s32 rd5, r4, 16;  // sizeof(vec4)*(blockDim.x*blockIdx.x + threadIdx.x)
         add.s64 rd6, rd4, rd5;     // param0 + sizeof(vec4)*(blockDim.x*blockIdx.x + threadIdx.x)
@@ -68,6 +68,69 @@ char *generate_ptx_program(frep_t *f, size_t *out_length)
     stream += sprintf(stream, ptx_template, result_register, result_register, ptx);
     *out_length = (stream - buffer);
     return buffer;
+}
+
+CUmodule load_ptx_program(
+    const char *ptx_source, size_t ptx_source_length,
+    int jit_optimization_level)
+{
+    CUmodule module;
+    void *cubin; size_t cubin_size;
+    CUlinkState link_state;
+    enum { num_options = 8 };
+    CUjit_option options[num_options];
+    void *option_values[num_options];
+    float walltime;
+    char error_log[8192], info_log[8192];
+
+    assert(jit_optimization_level >= 0 && jit_optimization_level <= 4);
+
+    // see CUDA Driver API manual for these options (look up cuLinkCreate)
+    options[0] = CU_JIT_WALL_TIME;                   option_values[0] = (void *) &walltime;
+    options[1] = CU_JIT_INFO_LOG_BUFFER;             option_values[1] = (void *) info_log;
+    options[2] = CU_JIT_INFO_LOG_BUFFER_SIZE_BYTES;  option_values[2] = (void *) (long)sizeof(info_log);
+    options[3] = CU_JIT_ERROR_LOG_BUFFER;            option_values[3] = (void *) error_log;
+    options[4] = CU_JIT_ERROR_LOG_BUFFER_SIZE_BYTES; option_values[4] = (void *) (long)sizeof(error_log);
+    options[5] = CU_JIT_LOG_VERBOSE;                 option_values[5] = (void *) 1;
+    options[6] = CU_JIT_TARGET;                      option_values[6] = (void *) CU_TARGET_COMPUTE_60;
+    options[7] = CU_JIT_OPTIMIZATION_LEVEL;          option_values[7] = (void *) (long)jit_optimization_level;
+    cudaCheckError(cuLinkCreate(num_options, options, option_values, &link_state));
+
+    int err = cuLinkAddData(link_state, CU_JIT_INPUT_PTX, (void *)ptx_source, ptx_source_length+1, 0, 0, 0, 0);
+    if (err != CUDA_SUCCESS)
+        fprintf(stderr, "PTX Linker Error:\n%s\n", error_log);
+    cudaCheckError(cuLinkComplete(link_state, &cubin, &cubin_size));
+    printf("Linking done in %fms. Linker Output:\n%s\n", walltime, info_log);
+
+    cudaCheckError(cuModuleLoadData(&module, cubin)); assert(module);
+    cudaCheckError(cuLinkDestroy(link_state));
+    return module;
+}
+
+void run_ptx_program(
+    void *input, size_t sizeof_input,
+    void *output, size_t sizeof_output,
+    const char *ptx_source, size_t ptx_source_length, const char *entry_name,
+    int num_blocks, int threads_per_block, int shared_memory_bytes=1024,
+    int jit_optimization_level=1 /*allowed values = 0,1,2,3,4*/)
+{
+    CUdeviceptr dev_input;
+    CUdeviceptr dev_output;
+    cudaCheckError(cuMemAlloc(&dev_input, sizeof_input)); assert(dev_input);
+    cudaCheckError(cuMemAlloc(&dev_output, sizeof_output)); assert(dev_output);
+    cudaCheckError(cuMemcpyHtoD(dev_input, input, sizeof_input));
+    CUmodule module = load_ptx_program(ptx_source, ptx_source_length, jit_optimization_level);
+    CUfunction kernel = 0;
+    cudaCheckError(cuModuleGetFunction(&kernel, module, entry_name));
+    uint64_t param0 = (uint64_t)(dev_input);
+    uint64_t param1 = (uint64_t)(dev_output);
+    void *kernel_params[] = { (void*)&param0, (void*)&param1 };
+    cuLaunchKernel(kernel, num_blocks,1,1, threads_per_block,1,1, shared_memory_bytes, NULL, kernel_params, NULL);
+    cudaCheckError(cuCtxSynchronize());
+    cudaCheckError(cuMemcpyDtoH(output, dev_output, sizeof_output));
+    cudaCheckError(cuMemFree(dev_output));
+    cudaCheckError(cuMemFree(dev_input));
+    cudaCheckError(cuModuleUnload(module));
 }
 
 void run_test(int test_number, frep_t *f)
@@ -118,11 +181,12 @@ void run_test(int test_number, frep_t *f)
     {
         size_t ptx_length;
         char *ptx_source = generate_ptx_program(f, &ptx_length);
-        RunPTX(input, sizeof_input,
-               output, sizeof_output,
-               ptx_source, ptx_length,
-               "main",
-               num_blocks, num_threads);
+        run_ptx_program(
+            input, sizeof_input,
+            output, sizeof_output,
+            ptx_source, ptx_length,
+            "main",
+            num_blocks, num_threads);
     }
 
     // verify that GPU output matches CPU output
@@ -149,6 +213,25 @@ void run_test(int test_number, frep_t *f)
 
 int main(int argc, char **argv)
 {
+    // disable CUDA from caching SASS programs
+    setenv("CUDA_CACHE_DISABLE", "1", 1);
+
+    CUcontext context;
+    CUdevice device;
+    cudaCheckError(cuInit(0));
+    cudaCheckError(cuDeviceGet(&device, 0));
+    cudaCheckError(cuCtxCreate(&context, 0, device));
+
+    char name[256];
+    int major = 0, minor = 0;
+    int compute_mode = -1;
+    cudaCheckError(cuDeviceGetName(name, 100, device));
+    cudaCheckError(cuDeviceGetAttribute(&major, CU_DEVICE_ATTRIBUTE_COMPUTE_CAPABILITY_MAJOR, device));
+    cudaCheckError(cuDeviceGetAttribute(&minor, CU_DEVICE_ATTRIBUTE_COMPUTE_CAPABILITY_MINOR, device));
+    cudaCheckError(cuDeviceGetAttribute(&compute_mode, CU_DEVICE_ATTRIBUTE_COMPUTE_MODE, device));
+    assert(compute_mode != CU_COMPUTEMODE_PROHIBITED && "Device is running in Compute Mode Prohibited");
+    printf("Using CUDA device %s: Compute SM %d.%d\n", name, major, minor);
+
     frep_t *f = fBoxCheap(1.0f, 0.5f, 0.25f);
     run_test(1, f);
 
