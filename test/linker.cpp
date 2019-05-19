1cc60a8d4093e22906c0c683819e6d2cd6bbe039
lightbits <simenh.jobb@gmail.com>
2019-05-19 19:06:47 +0200

    comments

--- test/linker.cpp
+++ test/linker.cpp
@@ -1,11 +1,12 @@
 // This file tests the use of seperate compilation to link together
-// pre-existing Cubin files. This is useful because it means we can
-// use the CUDA Driver API to generate an executable Cubin from the
-// SASS backend and an externally provided Cubin containing a "main"
-// entrypoint.
-
-// Compilation instructions (Linux, g++):
-// g++ -std=c++11 linker.cpp -I/usr/local/cuda-10.1/include -lcuda
+// pre-existing (relocatable) Cubin files. This is useful because we
+// can use the CUDA Driver API to generate an executable Cubin from
+// the output of our SASS backend and a user-provided Cubin containing
+// the entrypoint.
+//
+// To compile this file on Linux using g++:
+// $ g++ -std=c++11 linker.cpp -I/usr/local/cuda-10.1/include -lcuda
+//
 #include <stdio.h>
 #include <assert.h>
 #include <cuda.h>
@@ -18,12 +19,19 @@
 int main() {
     init_cuda();
 
+    //
+    // Generate relocatable SASS binaries by invoking the PTX assembler
+    // on our two test files. Neither of these can be executed on their
+    // own, so we will link them together into an actual executable using
+    // the CUDA linker in the Driver API.
+    //
     system("/usr/local/cuda-10.1/bin/ptxas --opt-level 1 --compile-only --gpu-name sm_60 test1.ptx --output-file test1.cubin");
     system("/usr/local/cuda-10.1/bin/ptxas --opt-level 1 --compile-only --gpu-name sm_60 test2.ptx --output-file test2.cubin");
 
     int sizeof_cubin1 = 0;
     void *cubin1 = (void*)read_file("test1.cubin", &sizeof_cubin1);
     assert(cubin1);
+
     int sizeof_cubin2 = 0;
     void *cubin2 = (void*)read_file("test2.cubin", &sizeof_cubin2);
     assert(cubin2);
@@ -31,44 +39,58 @@ int main() {
     CUfunction kernel;
     CUmodule module;
     const char *entry_name = "main";
+
+    // We do this 100 times and measure the time it takes the driver to
+    // link together the Cubin file, and report the average in ms.
     for (int i = 0; i < 100; i++)
     {
         TIMING("linker");
-        void *cubin; size_t cubin_size;
-        CUlinkState link_state;
+
+        //
+        // initialize the linker. note: CU_JIT_TARGET must match compute mode
+        // specified in test1.ptx and test2.ptx, and the --gpu-name argument
+        // passed to ptxas above.
+        //
         enum { num_options = 6 };
         CUjit_option options[num_options];
         void *option_values[num_options];
-        char error_log[8192], info_log[8192];
-
-        // see CUDA Driver API manual for these options (look up cuLinkCreate)
+        char error_log[8192];
+        char info_log[8192];
         options[0] = CU_JIT_INFO_LOG_BUFFER;             option_values[0] = (void *) info_log;
         options[1] = CU_JIT_INFO_LOG_BUFFER_SIZE_BYTES;  option_values[1] = (void *) (long)sizeof(info_log);
         options[2] = CU_JIT_ERROR_LOG_BUFFER;            option_values[2] = (void *) error_log;
         options[3] = CU_JIT_ERROR_LOG_BUFFER_SIZE_BYTES; option_values[3] = (void *) (long)sizeof(error_log);
         options[4] = CU_JIT_LOG_VERBOSE;                 option_values[4] = (void *) 1;
         options[5] = CU_JIT_TARGET;                      option_values[5] = (void *) CU_TARGET_COMPUTE_60;
+        CUlinkState link_state;
         cudaCheckError(cuLinkCreate(num_options, options, option_values, &link_state));
 
-        {
-            int err = cuLinkAddData(link_state, CU_JIT_INPUT_CUBIN, (void *)cubin1, sizeof_cubin1, 0, 0, 0, 0);
-            if (err != CUDA_SUCCESS)
-                fprintf(stderr, "PTX Linker Error:\n%s\n", error_log);
-        }
-        {
-            int err = cuLinkAddData(link_state, CU_JIT_INPUT_CUBIN, (void *)cubin2, sizeof_cubin2, 0, 0, 0, 0);
-            if (err != CUDA_SUCCESS)
-                fprintf(stderr, "PTX Linker Error:\n%s\n", error_log);
-        }
+        if (CUDA_SUCCESS != cuLinkAddData(link_state, CU_JIT_INPUT_CUBIN,
+            (void *)cubin1, sizeof_cubin1, 0,0,0,0))
+            fprintf(stderr, "nvlink error:\n%s\n", error_log);
+
+        if (CUDA_SUCCESS != cuLinkAddData(link_state, CU_JIT_INPUT_CUBIN,
+            (void *)cubin2, sizeof_cubin2, 0,0,0,0))
+            fprintf(stderr, "nvlink error:\n%s\n", error_log);
+
+        void *cubin;
+        size_t cubin_size;
         cudaCheckError(cuLinkComplete(link_state, &cubin, &cubin_size));
+
         cudaCheckError(cuModuleLoadData(&module, cubin)); assert(module);
         cudaCheckError(cuLinkDestroy(link_state));
         cudaCheckError(cuModuleGetFunction(&kernel, module, entry_name)); assert(kernel);
+
         TIMING("linker");
     }
     assert(kernel);
+
+    // Print the average linking time in milliseconds
     TIMING_SUMMARY();
 
+    //
+    // finally we run the thing to make sure that it actually works.
+    //
     int N = 32;
     size_t sizeof_input = N*sizeof(float);
     size_t sizeof_output = N*sizeof(float);
@@ -77,7 +99,6 @@ int main() {
     int num_blocks = 8;
     int num_threads = 4;
     int shared_memory_bytes = 1024;
-
     CUdeviceptr dev_input;
     CUdeviceptr dev_output;
     cudaCheckError(cuMemAlloc(&dev_input, sizeof_input)); assert(dev_input);
