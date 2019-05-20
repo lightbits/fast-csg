7bcd6933c496ae919d6bb0f379a432ccc9d71b86
lightbits <email-omitted>
2019-05-20 16:14:48 +0200

    add cubin files

--- test/linker.cpp
+++ test/linker.cpp
@@ -92,10 +92,19 @@ int main() {
     // finally we run the thing to make sure that it actually works.
     //
     int N = 32;
-    size_t sizeof_input = N*sizeof(float);
+    size_t sizeof_input = 4*N*sizeof(float);
     size_t sizeof_output = N*sizeof(float);
     float *input = (float*)malloc(sizeof_input);
     float *output = (float*)malloc(sizeof_output);
+
+    for (int i = 0; i < 32; i++)
+    {
+        input[4*i + 0] = 1.1f;
+        input[4*i + 1] = 0.0f;
+        input[4*i + 2] = 0.0f;
+        input[4*i + 3] = 0.0f;
+    }
+
     int num_blocks = 8;
     int num_threads = 4;
     int shared_memory_bytes = 1024;
